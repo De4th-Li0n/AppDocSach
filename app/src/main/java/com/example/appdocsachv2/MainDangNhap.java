a87b465b4662544ba510ea4257cae2b61e9239b3
De4th-Li0n <73570727+De4th-Li0n@users.noreply.github.com>
2021-08-20 16:09:16 +0700

    1 xong

---
+++ app/src/main/java/com/example/appdocsachv2/MainDangNhap.java
@@ -0,0 +1,14 @@
+package com.example.appdocsachv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dang_nhap);
+    }
+}
